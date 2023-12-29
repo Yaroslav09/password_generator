da4f29f2e6cbe7b69155b1d354d70e1587d0e162
Yaroslav Bozhak <bozhakyaroslav@gmail.com>
2023-12-29 23:43:58 +0000

    added functionality in html and js file to generate a random password

--- script.js
+++ script.js
@@ -88,31 +88,103 @@ var upperCasedCharacters = [
   'Z'
 ];
 
-// Function to prompt user for password options
-function getPasswordOptions() {
-
-}
-
 // Function for getting a random element from an array
-function getRandom(arr) {
-
+function getRandomUpper() {
+  var randomIndex = Math.floor(Math.random() * upperCasedCharacters.length);
+  return upperCasedCharacters[randomIndex];  
 }
 
-// Function to generate password with user input
-function generatePassword() {
-
+function getRandomLower() {
+  var randomIndex = Math.floor(Math.random() * lowerCasedCharacters.length);
+  return lowerCasedCharacters[randomIndex]; 
 }
 
-// Get references to the #generate element
-var generateBtn = document.querySelector('#generate');
+function getRandomNumeric() {
+  var randomIndex = Math.floor(Math.random() * numericCharacters.length);
+  return numericCharacters[randomIndex];
+}
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector('#password');
+function getRandomSymbol() {
+  var randomIndex = Math.floor(Math.random() * specialCharacters.length);
+  return specialCharacters[randomIndex];
+}
 
-  passwordText.value = password;
+// Get references to the element
+const resultEl = document.getElementById('password')
+const lengthEl = document.getElementById('length')
+const uppercaseEl = document.getElementById('uppercase')
+const lowercaseEl = document.getElementById('lowercase')
+const numericEl = document.getElementById('numeric')
+const symbolsEl = document.getElementById('symbols')
+const generateBtn = document.getElementById('generate')
+const clipboardEl = document.getElementById('clipboard')
+
+const randomFunc = {
+    upper: getRandomUpper,
+    lower: getRandomLower,
+    numeric: getRandomNumeric,
+    symbol: getRandomSymbol
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener('click', writePassword);
+generateBtn.addEventListener('click', () => {
+  const length = +lengthEl.value  
+  const hasUpper = uppercaseEl.checked
+  const hasLower = lowercaseEl.checked
+  const hasNum = numericEl.checked
+  const hasSymbol = symbolsEl.checked
+
+  resultEl.innerText = generatePassword(hasUpper, hasLower, hasNum, hasSymbol, length)  
+})
+
+// Function to generate password
+function generatePassword(upper, lower, numeric, symbol, length) {
+  let generatedPassword = ''
+  const typesCount = upper + lower + numeric + symbol
+  const typesArr = [{upper}, {lower}, {numeric}, {symbol}].filter(item => Object.values(item)[0])
+
+  if(typesCount === 0) {
+      return 'At least one character type should be selected'
+  }
+
+  for(let i=0; i < length; i += typesCount) {
+      typesArr.forEach( type => {
+          const funcName = Object.keys(type)[0]
+          generatedPassword += randomFunc[funcName]()            
+      })
+  }
+  
+  const finalPassword = generatedPassword.slice(0, length)
+  return finalPassword
+}
+
+// Function to copy password to clipboard
+function copyToClipboard() {
+  const password = resultEl.value.trim();
+
+  if (!password) {
+    alert('Generate a password first before copying.');
+    return;
+  }
+
+  navigator.clipboard.writeText(password)
+    .then(() => {
+      alert('Password copied to clipboard');
+    })
+    .catch(err => {
+      console.error('Unable to copy to clipboard', err);
+      alert('Failed to copy password to clipboard. Please try again.');
+    });
+}
+
+clipboardEl.addEventListener('click', copyToClipboard);
+
+// Range Sliders
+document.addEventListener('DOMContentLoaded', function () {
+  const lengthInput = document.getElementById('length');
+  const lengthValueSpan = document.getElementById('lengthValue');  
+
+  lengthInput.addEventListener('input', function () {
+    lengthValueSpan.textContent = lengthInput.value;
+  });  
+});
